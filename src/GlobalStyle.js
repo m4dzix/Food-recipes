535a8845b7228a4184bead6548ce5b4f6b4977c0
Madlen <madlen@SoftRay.local>
2021-04-29 17:46:39 +0200

    Added padding and margin 0 to body instead of html

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,13 +4,13 @@ export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&display=swap');
 html{
   box-sizing: border-box;
-  padding: 0;
-  margin: 0;
 }
 *,::before, ::after{
   box-sizing: inherit;
 }
 body{
   font-family: 'Montserrat', sans-serif;
+  padding: 0;
+  margin: 0;
 }
 `;
