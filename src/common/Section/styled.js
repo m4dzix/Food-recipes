36fee1d871dce021fa7774cc7745d7107838f3e1
Madlen <madlen@SoftRay.local>
2021-08-22 15:32:54 +0200

    Changed grid template rows on lower resolutions

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 export const StyledSection = styled.section`
   background-color: #1118;
   padding: 50px;
-  @media (max-width: 400px) {
+  @media (max-width: 430px) {
     padding: 10px;
   }
   ${({ list }) =>
@@ -17,9 +17,11 @@ export const StyledSection = styled.section`
       justify-content: center;
       @media (max-width: 830px) and (orientation: landscape) {
         grid-template-columns: repeat(auto-fill, minmax(224px, 1fr));
+        grid-template-rows: auto 1fr;
       }
       @media (max-width: 768px) {
         grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+        grid-template-rows: auto 1fr;
       }
       @media (max-width: 430px) and (orientation: portrait) {
         grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
