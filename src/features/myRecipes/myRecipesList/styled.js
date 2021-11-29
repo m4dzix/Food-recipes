8ef0117140c6baa3e27b9e96548c8a0354440a61
Magdalena Checinski <madlen.checinski@gmail.com>
2021-11-29 13:15:32 +0100

    Added and improved styles

--- src/features/myRecipes/myRecipesList/styled.js
+++ src/features/myRecipes/myRecipesList/styled.js
@@ -8,6 +8,9 @@ export const Title = styled.h1`
   background-color: whitesmoke;
   margin-bottom: 0;
   padding: 50px 30px 25px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 40px;
+  }
 `;
 export const List = styled.ul`
   margin-top: 0;
@@ -15,15 +18,17 @@ export const List = styled.ul`
   background-position: fixed;
   list-style: none;
   padding: 25px 30px 50px;
-  height: 65vh;
+  height: 100vh;
   overflow: scroll;
-  display: flex;
-  flex-direction: column;
+  align-items: flex-start;
 `;
-export const Item = styled.li`
-  font-size: 30px;
+export const Name = styled.p`
+  position: static;
+  font-size: 35px;
+  align-self: end;
   font-weight: bold;
-  padding: 15px;
+  padding-left: 15px;
+  margin: 10px;
   width: fit-content;
   transition: 0.5s ease;
   padding-bottom: 2px;
@@ -32,8 +37,52 @@ export const Item = styled.li`
   background-size: 0% 3px;
   background-repeat: no-repeat;
   transition: 0.5s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 30px;
+  }
+`;
+export const Item = styled.li`
+  border: 2px solid black;
+  box-shadow: 0px 0px 5px teal;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  transition: 0.5s;
+  position: static;
+  margin-bottom: 20px;
   &:hover {
+    border: 4px solid teal;
+    transform: scale(1.005);
+  }
+  &:hover ${Name} {
     background-size: 100% 3px;
-    font-size: 32px;
+    font-size: 35px;
+  }
+`;
+export const Options = styled.div`
+  display: flex;
+  justify-self: end;
+  align-items: end;
+  margin: 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    flex-direction: column;
+  }
+`;
+export const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  font-size: 20px;
+  padding: 0px 10px;
+  transition: 0.5s;
+  position: static;
+  &:hover {
+    color: teal;
+    font-size: 22px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 18px;
+    &:hover {
+      font-size: 20px;
+    }
   }
 `;
