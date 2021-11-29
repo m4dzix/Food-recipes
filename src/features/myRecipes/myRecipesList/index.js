4a3487bcec470324aa417f48f8068c5c9c27ea1a
Magdalena Checinski <madlen.checinski@gmail.com>
2021-11-29 17:29:20 +0100

    Appearance of the subpage without recipes

--- src/features/myRecipes/myRecipesList/index.js
+++ src/features/myRecipes/myRecipesList/index.js
@@ -1,42 +1,77 @@
 import React from "react";
 import Section from "../../../common/Section";
-import { Title, List, Item, Name, Options, Button } from "./styled";
+import {
+  Title,
+  Container,
+  Item,
+  MainInformation,
+  Options,
+  Button,
+} from "./styled";
 import { useSelector, useDispatch } from "react-redux";
 import { selectMyRecipes, removeRecipe } from "../myRecipesSlice";
+import { Link } from "react-router-dom";
+import { toAddRecipe } from "../../../routes";
 
 const MyRecipeList = () => {
   const recipeList = useSelector(selectMyRecipes);
   const dispatch = useDispatch();
   return (
     <Section myRecipe={"true"}>
-      <Title>My recipe list</Title>
-      <List>
-        {recipeList.map(({ name }) => (
-          <Item key={name}>
-            <Name>{name}</Name>
-            <Options>
-              <Button>
-                Go to recipe
-                <span as="img" alt="aria-label">
-                  {" "}
-                  ➡️{" "}
-                </span>
-              </Button>
-              <Button
-                onClick={() => {
-                  dispatch(removeRecipe(name));
-                }}
-              >
-                Delete
-                <span as="img" alt="aria-label">
-                  {" "}
-                  🚫{" "}
-                </span>
-              </Button>
-            </Options>
-          </Item>
-        ))}
-      </List>
+      {recipeList.length !== 0 ? (
+        <>
+          <Title>My recipe list</Title>
+          <Container list={"true"} as="ul">
+            {recipeList.map(({ name }) => (
+              <Item key={name}>
+                <MainInformation>{name}</MainInformation>
+                <Options>
+                  <Button>
+                    Go to recipe
+                    <span as="img" alt="aria-label">
+                      {" "}
+                      ➡️{" "}
+                    </span>
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      dispatch(removeRecipe(name));
+                    }}
+                  >
+                    Delete
+                    <span as="img" alt="aria-label">
+                      {" "}
+                      🚫{" "}
+                    </span>
+                  </Button>
+                </Options>
+              </Item>
+            ))}
+          </Container>
+        </>
+      ) : (
+        <Container>
+          <Title>
+            You dont have any recipe...{" "}
+            <span as="img" alt="aria-label">
+              {" "}
+              😳{" "}
+            </span>
+          </Title>
+          <MainInformation noRecipe="true">
+            Click on button to add recipe:{" "}
+          </MainInformation>
+          <Link to={toAddRecipe()}>
+            <Button noRecipe="true">
+              Add recipe
+              <span as="img" alt="aria-label">
+                {" "}
+                📃 🥘{" "}
+              </span>
+            </Button>
+          </Link>
+        </Container>
+      )}
     </Section>
   );
 };
--- src/features/myRecipes/myRecipesList/styled.js
+++ src/features/myRecipes/myRecipesList/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Title = styled.h1`
   font-size: 50px;
@@ -12,17 +12,28 @@ export const Title = styled.h1`
     font-size: 40px;
   }
 `;
-export const List = styled.ul`
+export const Container = styled.div`
   margin-top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
   background-color: whitesmoke;
   background-position: fixed;
   list-style: none;
-  padding: 25px 30px 50px;
+  padding: 50px 30px;
   height: 100vh;
   overflow: scroll;
-  align-items: flex-start;
+  ${({ list }) =>
+    list &&
+    css`
+      list-style: none;
+      overflow: scroll;
+      align-items: center;
+      padding: 25px 30px 50px;
+    `}
 `;
-export const Name = styled.p`
+export const MainInformation = styled.p`
   position: static;
   font-size: 35px;
   align-self: end;
@@ -37,10 +48,14 @@ export const Name = styled.p`
   background-size: 0% 3px;
   background-repeat: no-repeat;
   transition: 0.5s;
-
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 30px;
   }
+  ${({ noRecipe }) =>
+    noRecipe &&
+    css`
+      align-self: unset;
+    `}
 `;
 export const Item = styled.li`
   border: 2px solid black;
@@ -50,11 +65,12 @@ export const Item = styled.li`
   transition: 0.5s;
   position: static;
   margin-bottom: 20px;
+  width: 100%;
   &:hover {
     border: 4px solid teal;
     transform: scale(1.005);
   }
-  &:hover ${Name} {
+  &:hover ${MainInformation} {
     background-size: 100% 3px;
     font-size: 35px;
   }
@@ -85,4 +101,16 @@ export const Button = styled.button`
       font-size: 20px;
     }
   }
+  ${({ noRecipe }) =>
+    noRecipe &&
+    css`
+      align-self: unset;
+      border: 1px solid black;
+      border-radius: 5px;
+      background-color: teal;
+      padding: 5px 10px;
+      &:hover {
+        color: white;
+      }
+    `}
 `;
