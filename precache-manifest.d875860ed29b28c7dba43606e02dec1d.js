7dc656c978dae49bca1fb4255ccac38f380c81ef
Magdalena Checinski <madlen.checinski@gmail.com>
2021-12-20 17:01:45 +0100

    Updates

---
+++ precache-manifest.d875860ed29b28c7dba43606e02dec1d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ca4d30f770873edbc17",
+    "url": "/food-recipes/static/js/main.20a7074e.chunk.js"
+  },
+  {
+    "revision": "e1faba5fce0b71b91b65",
+    "url": "/food-recipes/static/js/runtime~main.cc58faf8.js"
+  },
+  {
+    "revision": "ecfcd478b9bbd081406c",
+    "url": "/food-recipes/static/js/2.4051ff76.chunk.js"
+  },
+  {
+    "revision": "666eb2b96bdfcfe59711",
+    "url": "/food-recipes/static/js/3.034e91e1.chunk.js"
+  },
+  {
+    "revision": "0898e05567b8cce87b663ed610c50f5c",
+    "url": "/food-recipes/static/media/logo.0898e055.png"
+  },
+  {
+    "revision": "dd14bd7cd95d7a22759a8607ffee844c",
+    "url": "/food-recipes/static/media/danger.dd14bd7c.svg"
+  },
+  {
+    "revision": "8a8ec077c2e7fe6a6ae890a6cb2b02e9",
+    "url": "/food-recipes/static/media/greekFood.8a8ec077.jpg"
+  },
+  {
+    "revision": "d4eefd544a8ea2536fbfa5ed3ed600e5",
+    "url": "/food-recipes/static/media/greekLandscape.d4eefd54.jpg"
+  },
+  {
+    "revision": "ec42a325e6ef683d2b3a952260459cd0",
+    "url": "/food-recipes/static/media/italyFood.ec42a325.jpg"
+  },
+  {
+    "revision": "aa7068ace8afab8109c864612f9062dd",
+    "url": "/food-recipes/static/media/italyLandscape.aa7068ac.jpg"
+  },
+  {
+    "revision": "6f82d7b12375c79784fb25cd903b7d0b",
+    "url": "/food-recipes/static/media/mexicoFood.6f82d7b1.jpg"
+  },
+  {
+    "revision": "cf04eb968ad04ba0b5c8f5f9637d804f",
+    "url": "/food-recipes/static/media/mexicoLandscape.cf04eb96.jpg"
+  },
+  {
+    "revision": "f024f05645127fe7a43f4f74ac3c3278",
+    "url": "/food-recipes/static/media/asiaFood.f024f056.jpg"
+  },
+  {
+    "revision": "6a61040069c40bc8a5dd8759b8195ff5",
+    "url": "/food-recipes/static/media/asiaLandscape.6a610400.jpg"
+  },
+  {
+    "revision": "ab5b22d5709598c2a64966a1fdff6dd1",
+    "url": "/food-recipes/static/media/loader.ab5b22d5.svg"
+  },
+  {
+    "revision": "160816b9eb7b2b22a8a383ed7b7a1378",
+    "url": "/food-recipes/index.html"
+  }
+];
